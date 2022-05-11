c009e6d0f92510b708d0edffa054c217a3cad0d8
David Čeliš <u19.404@pslib.cloud>
2022-05-11 10:26:37 +0200

    2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import GetGame from './Components/GetGame';
 import {Container} from "reactstrap";
 import PostGame from './Components/PostGame';
-import DeleteGame from './Components/DeleteGame';
+
 
 function App() {
   return (
@@ -13,8 +13,6 @@ function App() {
       <GetGame/>
       <h2>Add new game</h2>
       <PostGame/>
-      <h2>Delete game</h2>
-      <DeleteGame/>
       </Container>
     </div>
   )
--- src/Components/GetGame.js
+++ src/Components/GetGame.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
   export default function GetGame() {
+
       const[loading, setLoading]= useState(true);
       const[games, setGames]= useState([]);
-      axios.get(`http://localhost:54434/api/Game`)
+      const Delete= (id) => {
+        axios.delete(`http://localhost:54434/api/Game/${id}`)
+        window.location.reload();
+      }
+      useEffect(()=>{
+        axios.get(`http://localhost:54434/api/Game`)
         .then(res => {
           const games = res.data;
           setGames(games)
           setLoading(false);
         })
-        .catch(error => {this.setState({errorMessage: error.message});
-        console.error("Error!", error);
-      });
+      },[]) 
+      // function UseFetch(){
+      //   axios.get(`http://localhost:54434/api/Game`)
+      //   .then(res => {
+      //     const games = res.data;
+      //     setGames(games)
+      //     setLoading(false);
+      //   })
+      // }
+        // .catch(error => {this.setState({errorMessage: error.message});
+        // console.error("Error!", error);
+     
 
-
+ 
     if(loading){
       return(<h1>Loading...</h1>)
     }
@@ -22,8 +37,14 @@ import axios from 'axios';
       <ul>
         {
           games
-            .map(game =>
-              <li key={game.GameId}>{game.GameName},{game.GenreId}</li>
+            .map(game => {
+              return(
+              <div key={game.GameId}>
+                <li key={game.GameId}>{game.GameName},{game.GenreId}</li>
+                <button onClick={() => Delete(game.GameId)}></button>
+              </div>
+              )
+              }
             )
         }
       </ul>
